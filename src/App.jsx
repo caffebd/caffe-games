8b9748ad93c3cd4bed1eb4140d11bcc198b48271
Luke Doyle <caffebd@gmail.com>
2023-10-10 19:06:03 +0600

    Stealth Games b

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Jams from "./pages/Jams";
 import VS from "./pages/Vs";
 import TenMinutes from "./pages/TenMinutes";
 import Stealth from "./pages/Stealth";
+import All from "./pages/All";
 function App() {
   const [darkTheme, setDarkTheme] = createSignal(false);
 
@@ -29,19 +30,26 @@ function App() {
         >
           light_mode
         </span> */}
-
+{/* 
         <A href="/">
           <h1 class="m-3 px-3 py-2 rounded bg-red-500 text-white text-2xl">
             CAFFE Games
           </h1>
-        </A>
+        </A> */}
         {/* <A href="/cart">Cart</A> */}
       </header>
+      <A href="/">
       <img
         class="rounded-md"
         src="/images/caffe_games_banner.png"
         alt="CAFFE Games Banner"
       />
+      </A>
+      <A href="/all">
+        <div class="border-double border-2 border-red-800 bg-red-500 py-1 px-2  mt-3 w-1/6 text-center rounded-sm shadow-md text-white">
+          See all games
+        </div>
+      </A>
 
       <Routes>
         <Route path="/" component={Home} />
@@ -51,6 +59,7 @@ function App() {
         <Route path="/cart" component={Cart} />
         <Route path="/ten-minute" component={TenMinutes} />
         <Route path="/stealth" component={Stealth} />
+        <Route path="/all" component={All} />
         <Route path="/game/:short" component={Game} />
       </Routes>
     </div>
